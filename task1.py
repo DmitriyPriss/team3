fe784bd18b5362cfa1cf4c5544ffa553e75bf127
priss.dv <dmitrii.priss@domru.ru>
2020-11-03 16:40:39 +0500

    1) complete

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
-qwe
+A = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+B = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+D = ["", "M", "MM", "MMM"]
+
+print("Введите арабское число")
+number = int(input())
+print(D[number // 1000]+C[number % 1000 // 100]+B[number % 100 // 10]+A[number % 10])
